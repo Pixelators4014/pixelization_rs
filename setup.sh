680545e3d20fc8751aedc1c8f96c9256482e266e
Ashwin Naren <arihant2math@gmail.com>
2024-03-18 22:12:12 -0700

    updates

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get autoremove -y
 
 # Rust setup
 sudo apt install -y git libclang-dev python3-pip python3-vcstool # libclang-dev is required by bindgen
-$ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none
 # Install these plugins for cargo and colcon:
 cargo install cargo-ament-build
 pip install git+https://github.com/colcon/colcon-cargo.git
@@ -16,3 +16,7 @@ pip install git+https://github.com/colcon/colcon-ros-cargo.git
 # Colcon build + install
 colcon build --symlink-install
 source install/setup.bash
+
+# More rust setup
+vcs import src < src/ros2_rust/ros2_rust_humble.repos
+. /opt/ros/humble/setup.sh
