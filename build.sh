1b808b6c46829ae92acd927861d1b3d3fc8108f3
Alistair Keiller <alistair@keiller.net>
2024-03-19 14:32:31 -0700

    test

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ mkdir -p ./usr/lib/aarch64-linux-gnu/
 mkdir -p ./usr/src/
 mkdir -p ./opt/nvidia/
 mkdir -p ./usr/share/
-cp /usr/bin/tegrastats ./usr/bin/
-cp /tmp/argus_socket ./tmp/
+cp -r /usr/bin/tegrastats ./usr/bin/
+cp -r /tmp/argus_socket ./tmp/
 cp /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11 ./usr/local/cuda-11.4/targets/aarch64-linux/lib/
 cp /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11 ./usr/local/cuda-11.4/targets/aarch64-linux/lib/
 cp /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10 ./usr/local/cuda-11.4/targets/aarch64-linux/lib/
@@ -16,9 +16,9 @@ cp /usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so ./usr/local/c
 cp /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4 ./usr/local/cuda-11.4/targets/aarch64-linux/lib/
 cp /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcudla.so.1 ./usr/local/cuda-11.4/targets/aarch64-linux/lib/
 cp /usr/local/cuda-11.4/targets/aarch64-linux/include/nvToolsExt.h ./usr/local/cuda-11.4/targets/aarch64-linux/include/
-cp /usr/lib/aarch64-linux-gnu/tegra ./usr/lib/aarch64-linux-gnu/
-cp /usr/src/jetson_multimedia_api ./usr/src/
-cp /opt/nvidia/nsight-systems-cli ./opt/nvidia/
-cp /opt/nvidia/vpi2 ./opt/nvidia/
-cp /usr/share/vpi2 ./usr/share/
+cp -r /usr/lib/aarch64-linux-gnu/tegra ./usr/lib/aarch64-linux-gnu/
+cp -r /usr/src/jetson_multimedia_api ./usr/src/
+cp -r /opt/nvidia/nsight-systems-cli ./opt/nvidia/
+cp -r /opt/nvidia/vpi2 ./opt/nvidia/
+cp -r /usr/share/vpi2 ./usr/share/
 docker build -t pixelization .
