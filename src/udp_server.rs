bfa9cba84b3d8e223e44b5b030a0f4d0bf9cc6ca
Ashwin Naren <arihant2math@gmail.com>
2024-03-23 11:17:49 -0700

    fixed mutex issue

--- src/udp_server.rs
+++ src/udp_server.rs
@@ -122,7 +122,7 @@ impl Server {
     async fn process_request(&mut self, request: Request) -> Response {
         return match request {
             Request::GetVslamPose => {
-                if let Some(msg) = self.data.lock().unwrap() {
+                if let Ok(msg) = self.data.lock() {
                     if let Some(last) = msg.poses.last() {
                         let now = now_millis_u31();
                         let header = (now - self.milli_start) as u32; // TODO: rework into return system
@@ -137,10 +137,10 @@ impl Server {
                         };
                         response.into()
                     } else {
-                        Response::Error("Server Error:No VSLAM data, please wait or check logs".to_string())
+                        Response::Error("Server Error: No VSLAM data, please wait or check logs".to_string())
                     }
                 } else {
-                    Response::Error("No VSLAM data".to_string())
+                    Response::Error("Server Error: Poisoned Mutex".to_string())
                 }
             }
             Request::SetVslamPose(pose) => {
