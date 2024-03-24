c9b40b75ab7cbe25740fadc2b9e2bde529656d57
Ashwin Naren <arihant2math@gmail.com>
2024-03-24 12:50:38 -0700

    fixed errors

--- src/udp_server.rs
+++ src/udp_server.rs
@@ -124,15 +124,15 @@ impl Server {
                 let service_request = isaac_ros_visual_slam_interfaces::srv::SetOdometryPose_Request {
                     pose: geometry_msgs::msg::Pose {
                         position: geometry_msgs::msg::Point {
-                            x: pose.x as f64,
-                            y: pose.y as f64,
-                            z: pose.z as f64,
+                            x: pose.position.x as f64,
+                            y: pose.position.y as f64,
+                            z: pose.position.z as f64,
                         },
                         orientation: geometry_msgs::msg::Quaternion {
-                            w: pose.angle_w as f64,
-                            x: pose.angle_x as f64,
-                            y: pose.angle_y as f64,
-                            z: pose.angle_z as f64,
+                            w: pose.orientation.w as f64,
+                            x: pose.orientation.x as f64,
+                            y: pose.orientation.y as f64,
+                            z: pose.orientation.z as f64,
                         }
                     }
                 };
