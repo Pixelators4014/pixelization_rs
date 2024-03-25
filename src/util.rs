90953c6677d97bb2e514541eb15935138a66b15e
Ashwin Naren <arihant2math@gmail.com>
2024-03-24 21:03:08 -0700

    compile time fix

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,6 @@
 use nalgebra::{Isometry3, Quaternion};
 
-pub fn pose_to_isometry(pose: &geometry_msgs::msg::Pose) {
+pub fn pose_to_isometry(pose: &geometry_msgs::msg::Pose) -> Isometry3<f32> {
     Isometry3::from_parts(
         nalgebra::Translation3::new(pose.position.x, pose.position.y, pose.position.z),
         nalgebra::base::Unit::new_normalize(Quaternion::new(pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z))
