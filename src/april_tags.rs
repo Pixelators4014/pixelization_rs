862a104490c10f5b874893928145a3d598bc1323
Ashwin Naren <arihant2math@gmail.com>
2024-03-24 21:13:05 -0700

    more bugs, ugh

--- src/april_tags.rs
+++ src/april_tags.rs
@@ -81,7 +81,7 @@ pub fn localize(detections: &AprilTagDetectionArray) -> Option<Isometry3<f32>> {
                 let inverse_robot_pose = relative_robot_pose.inverse();
                 let absolute_robot_pose = Isometry3::from_parts(
                     Translation3::from(april_tag_pose.translation.vector + inverse_robot_pose.translation.vector),
-                    nalgebra::geometry::Rotation3::from_euler_angles(april_tag_pose.rotation.euler_angles().0 + inverse_robot_pose.rotation.euler_angles().0, april_tag_pose.rotation.euler_angles().1 + inverse_robot_pose.rotation.euler_angles().1, april_tag_pose.rotation.euler_angles().2 + inverse_robot_pose.rotation.euler_angles().2),
+                    nalgebra::geometry::Rotation3::from_euler_angles(april_tag_pose.rotation.euler_angles().0 + inverse_robot_pose.rotation.euler_angles().0, april_tag_pose.rotation.euler_angles().1 + inverse_robot_pose.rotation.euler_angles().1, april_tag_pose.rotation.euler_angles().2 + inverse_robot_pose.rotation.euler_angles().2).into(),
                 );
                 let covariance = calc_abs_covariance(detection.pose.pose.covariance);
                 if covariance < lowest_covariance {
