9b0743d4423d1d61970788e1e501d70ab77e0a7b
Ashwin Naren <arihant2math@gmail.com>
2024-03-26 11:49:32 -0700

    updated launch shell scripts

--- run_dev.sh
+++ run_dev.sh
@@ -1,1 +1,11 @@
-docker run --privileged --network host -v /dev/*:/dev/* -v /etc/localtime:/etc/localtime:ro --runtime nvidia -it pixelization /bin/bash
+docker run -it --rm \
+    --privileged \
+    --network host \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -v /dev/*:/dev/* \
+    -v /etc/localtime:/etc/localtime:ro \
+    --runtime nvidia \
+     pixelization /bin/bash $@
