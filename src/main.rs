12651d1bb17e5fdcb52b6c92b191fc3168b73668
Ashwin Naren <arihant2math@gmail.com>
2024-03-26 22:30:39 -0700

    logging instead of eprintln

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ async fn run_ping(path_data: Arc<RwLock<Option<PathMsg>>>, april_tags_data: Arc<
 async fn main() -> Result<(), rclrs::RclrsError> {
     let context = rclrs::Context::new(std::env::args())?;
     let network_node = Arc::new(node::NetworkNode::new(&context)?);
-    ros2_logger::init(Arc::clone(&network_node.node)).unwrap();
+    ros2_logger::init_with_level(Arc::clone(&network_node.node), log::Level::Trace).unwrap();
     info!("Starting Pixelization Node");
     let server_path = Arc::clone(&network_node.path);
     let server_client = Arc::clone(&network_node.client);
