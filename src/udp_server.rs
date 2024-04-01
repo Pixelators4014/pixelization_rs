7bfa1f880cd18233af8a79b6f18251201a8ddb70
Ashwin Naren <arihant2math@gmail.com>
2024-03-31 19:35:47 -0700

    fixes

--- src/udp_server.rs
+++ src/udp_server.rs
@@ -73,7 +73,7 @@ enum Request {
     Shutdown,
 }
 
-#[derive(Copy, Clone, Error, Debug)]
+#[derive(Clone, Error, Debug)]
 pub enum ServerError {
     #[error("Request too short, expected at least 1 byte.")]
     TooShort,
@@ -109,7 +109,7 @@ impl Request {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 enum Response {
     Pose(Pose),
     Success,
