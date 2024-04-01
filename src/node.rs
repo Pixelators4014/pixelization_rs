2384148694bb5954d3734ad50f07258810801d16
Ashwin Naren <arihant2math@gmail.com>
2024-03-31 19:50:15 -0700

    fixes

--- src/node.rs
+++ src/node.rs
@@ -112,12 +112,12 @@ impl NetworkNode {
             if rx.has_changed().unwrap() {
                 let april_tags = rx.borrow_and_update();
                 trace!("April Tags: {april_tags:?}");
-                let april_tags_pose = april_tags::localize(april_tags);
+                let april_tags_pose = april_tags::localize(&april_tags);
                 if let Some(april_tags_pose) = april_tags_pose {
                     info!("Using April Tags Pose: {april_tags_pose:?}");
                     // TODO: impl kalman filter
                     let final_pose = april_tags_pose;
-                    let client = Arc::clone(&april_tags_client);
+                    let client = Arc::clone(&self.client);
                     let service_request =
                         isaac_ros_visual_slam_interfaces::srv::SetOdometryPose_Request {
                             pose: geometry_msgs::msg::Pose {
